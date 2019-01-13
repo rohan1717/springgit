5de8777fc3646987ad2a07135c42063e95e7b77f
AtDrive-PC4\AtDrive <rohan@atdrive.com>
2019-01-13 18:05:49 +0530

    Inital commit

---
+++ src/main/java/com/vega/springgit/SpringgitApplication.java
@@ -0,0 +1,14 @@
+package com.vega.springgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringgitApplication.class, args);
+    }
+
+}
+
